736f0ca984e0df4fe91a1e292b307f35296171fd
bavaaram <itsjustabavaar@gmail.com>
2023-04-11 04:01:33 +0330

    Optimizing the code and fix it's complexivity.

--- Sorting projects/Selection Sorting.py
+++ Sorting projects/Selection Sorting.py
@@ -1,19 +1,15 @@
 a = [10, 5, 3, 24, 2, 23, 17, 12, 1, 5, 9]
-
-for i in range(0, len(a)):
-    A = a[i]
-    B = a[i]
+n = len(a)
+for i in range(n):
+    selection = i
     flag = False
-    for j in range(i + 1, len(a)):
-        if a[j] == A:
+    for j in range(i + 1, n):
+        if a[j] == a[i]:
             continue
-        if a[j] < B:
+        if a[j] < a[selection]:
             flag = True     #if a smaller element than our i element through the i + 1 to end of list, flag is assigned to True and assignment between elements is needed.
-            B = a[j]
-            index = j
+            selection = j
+    a[i], a[selection] = a[selection], a[i]
     if not flag:        #if no element exists smaller than our element thet i counter is currently its index, no assignment needed and continue through the first loop.
         continue
-    a[i] = B
-    a[index] = A
-
 print(a)
